3463032b42f66b1844f28ec6f3ec373dfa6127e0
wuwenwen <wenwenwenwu@foxmail.com>
2020-06-08 17:42:01 +0800

    网络异常处理

---
+++ core/httpException.js
@@ -0,0 +1,17 @@
+class HttpException extends Error {
+  errorCode = 9999
+  statusCode = 500
+  message = ""
+
+  constructor(errorCode, message, statusCode) {
+      super()
+      this.errorCode = errorCode
+      this.message = message
+      this.statusCode = statusCode
+  }
+
+}
+
+export {
+  HttpException
+} 
--- utils/httpUtil.js
+++ utils/httpUtil.js
@@ -7,6 +7,9 @@ import {
 import {
   config
 } from '../config/config.js';
+import {
+  HttpException
+} from '../core/httpException.js';
 
 class HttpUtil {
 
@@ -29,16 +32,22 @@ class HttpUtil {
         }
       })
     } catch (e) {
+      if (throwError) {
+        throw new HttpException(-1, codes[-1])
+      }
       HttpUtil._showError(-1)
+      return null //抛出的话
     }
     const code = res.statusCode.toString()
     //请求成功
-    if (code.startsWith('2')) {
+    if (code.startsWith("2")) {
       return res.data
     } else {
       //token异常
-      if (code === '401') {
-        // 重新请求（只做一次）
+      //onLaunch中token未校验完成便执行了网络请求
+      //验证时可用的token使用时过期了
+      if (code === "401") {
+        //这两种极偶然的情况发生时，需要重新请求（只做一次）
         if (!data.refetched) {
           HttpUtil._refetch({
             url,
@@ -47,19 +56,24 @@ class HttpUtil {
           })
         }
       } else {
-        if (throwError) {
-          throw new HttpException(res.data.code, res.data.message, code)
+        if(throwError){
+          throw new HttpException(res.data.code,res.data.message,code)
         }
-        if (code === '404') {
+        //数据未找到
+        //数据有对象型和数组型两种，前端希望数据在有值和空值时返回的类型相同
+        //对象型空数据，服务端返回的原始值是UnifyResponse，需要转换为null
+        //数组型空数据，服务端返回的原始值是包含空数组的分页对象，直接返回即可
+        if (code === "404") { //数据未找到
+          //对象型数据
           if (res.data.code !== undefined) {
             return null
           }
+          //数组型数据
           return res.data
         }
         const error_code = res.data.code;
         HttpUtil._showError(error_code, res.data)
       }
-      // 403 404 500
     }
     return res.data
   }
